952a12354f70266e484aac25626ee75f87958400
이슬기 <iseulgizzz@gmail.com>
2022-05-19 00:23:13 +0900

    baekjoon 11866

--- baekjoon/11866.py
+++ baekjoon/11866.py
@@ -2,3 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
+queue = list(range(1, n+1))
+answer = []
+popindex = k-1
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.pop(0))
+    answer.append(queue.pop(0))
+
+print("<",end='')
+for i in range(len(answer)-1):
+    print("%d, "%answer[i], end='')
+print(answer[-1], end='')
+print(">")
